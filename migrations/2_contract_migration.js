0f89c380cda8af5367f1d9cd89d9e910520e8bf8
leekt216 <leekt216@gmail.com>
2020-03-19 20:23:20 +0900

    test/migration file

--- migrations/2_contract_migration.js
+++ migrations/2_contract_migration.js
@@ -1,6 +1,15 @@
 const fs = require("fs");
 const HiQuest = artifacts.require("HiQuest");
-
+const Token = artifacts.require("HiblocksMock");
 module.exports = (deployer, network, accounts) => {
-  deployer.deploy(HiQuest, accounts[0]);
+  let token;
+  let quest;
+  deployer.then( async ()=>{
+    token = await deployer.deploy(Token);
+  }).then(async()=>{
+    quest = await deployer.deploy(HiQuest, token.address);
+    await token.approve(quest.address, -1);
+    const receipt = await quest.create(web3.utils.randomHex(32), 0, 10000000000, 100);
+    console.log(receipt.logs[0].args);
+  });
 };
